f8c0cad909ddb30e0dfbc3522b4a6986db0c14b7
khru <evrtrabajo@gmail.com>
2014-12-03 17:42:05 +0100

    Update EjercicioEjemplo.sql

--- EjercicioEjemplo.sql
+++ EjercicioEjemplo.sql
@@ -2,31 +2,30 @@ Ejercicio de ejemplo:
 //------------------------------------------------------------------------------------------------------------------------------------------
 //La tabla persona se tiene que hacer primero porque no tiene foring key
 
-create tabla persona (
+create table persona (
 		DNI varchar(11) not null,
-		NSS varchar(14) not null,	//Tiene que tener las mismas caracteristicas que la clave primaria, es decir ha de ser UNICA.
+		NSS varchar(14) not null,		//Tiene que tener las mismas caracteristicas que la clave primaria, es decir ha de ser UNICA.
 		nombre varchar(50) not null,
 		apellido varchar(100) not null,
 		telf varchar(13) not null,
 		movil varchar(10),
 		primary key (DNI),
-		unique(NSS)	//Crear NSS como UNICA porque es una clave secundaria(clave candidata)		
+		unique(NSS)						//Crear NSS como UNICA porque es una clave secundaria(clave candidata)		
 		);
 
-create tabla asignatura (
+create table asignatura (
 		cod_asign integer not null, 
 		descripcion varchar(255), 
 		creditos integer not null, 
 		aula varchar(3), 
 		primary key(cod_asign));
 // Se ha de crear la ultima porque tiene una clave primaria compuesta por claves de otras tablas
-
-create tabla matricula (
+create table matricula (
 		DNI varchar(11) not null,
 		cod_asign integer not null,
 		curso varchar(9) not null,
 		primary key (DNI, cod_asign, curso), // clave compuesta por 3 campor
-		foreign key (cod_asign) references asignatura(cod_asign) on update cascade );//Se ha de decir que campo es el de la clave que apunta a otra 
+		foreign key (cod_asign) references asignatura(cod_asign) on update cascade );	//Se ha de decir que campo es el de la clave que apunta a otra 
 		//tabla y que tabla y campo es de la otra tabla
 		/* OPCIONES: 
 		"on delete"  o "on update"
